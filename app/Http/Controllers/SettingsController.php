61f59adfa99266eab7e9c48957b3410c4816c1f5
MarcoCouto <marcocouto90@gmail.com>
2017-07-10 14:29:20 +0000

    Define the exchanges period feature

---
+++ app/Http/Controllers/SettingsController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DB;
+use Illuminate\Http\Request;
+use App\Judite\Models\Settings;
+use App\Http\Requests\Settings\UpdateRequest;
+
+class SettingsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\Settings\UpdateRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateRequest $request)
+    {
+        $settings = DB::transaction(function () use ($request) {
+            $settings = Settings::first();
+            $settings->update($request->input());
+
+            return $settings;
+        });
+
+        return $settings;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
