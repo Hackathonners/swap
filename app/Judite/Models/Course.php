575e5fac26b00bc62dfc571a08dde27c8e576c3b
Bruno Ferraz <bruno.miguel.ni@gmail.com>
2017-08-08 21:43:27 +0000

    Add view students enrolled in a given course

--- app/Judite/Models/Course.php
+++ app/Judite/Models/Course.php
@@ -73,4 +73,14 @@ class Course extends Model
     {
         return $this->shifts()->where('tag', $tag)->first();
     }
+
+    /**
+     * Get enrollments of this course.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function enrollments()
+    {
+        return $this->hasMany(Enrollment::class);
+    }
 }
