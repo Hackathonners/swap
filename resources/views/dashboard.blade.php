95548ad872117400620228dc45dd9f5332287283
Francisco Neves <fntneves@gmail.com>
2017-08-09 20:04:06 +0100

    Hide exchanges blocks if exchanges period is inactive

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 <div class="container">
-        @if(!$proposedExchanges->isEmpty())
+        @if(!$proposedExchanges->isEmpty() && $settings->withinExchangePeriod())
             <div class="card mb-4">
                 <div class="card-header card-warning"><strong>Exchanges waiting for your confirmation</strong></div>
                 @include('exchanges.shared.requested.index', ['exchanges' => $proposedExchanges])
             </div>
         @endif
-        @if(!$requestedExchanges->isEmpty())
+        @if(!$requestedExchanges->isEmpty() && $settings->withinExchangePeriod())
             <div class="card mb-4">
                 <div class="card-header"><strong>Pending requested exchanges</strong></div>
                 <div class="card-table mb-0">
