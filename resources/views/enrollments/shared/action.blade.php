61919bc858088ec5cbc6a711d92da794e81f7d75
Francisco Neves <fntneves@gmail.com>
2017-08-09 17:24:16 +0100

    Fix logic of enrollment actions

--- resources/views/enrollments/shared/action.blade.php
+++ resources/views/enrollments/shared/action.blade.php
@@ -1,21 +1,19 @@
 <div class="btn-group">
+    {{-- Show button to exchange shift, if exchanges period is active --}}
+    @if ($settings->withinExchangePeriod())
+        <button type="button" class="btn btn-secondary btn-sm">Exchange shift</button>
+    @endif
 
-
-    @if (! Auth::user()->student->isEnrolledInCourse($course))
-        {{-- Show button to enroll in course. --}}
-        <form action="{{ route('enrollments.create') }}" method="post">
-            {{ csrf_field() }}
-            <input type="hidden" name="course_id" value="{{ $course->id }}">
-            <button type="submit" class="btn btn-success btn-sm">Enroll in course</button>
-        </form>
-    @else
-        {{-- Show button to exchange shift, if exchanges period is active --}}
-        @if ($settings->withinExchangePeriod())
-            <button type="button" class="btn btn-secondary btn-sm">Exchange shift</button>
-        @endif
-
-        {{-- Show dropdown to delete enrollment, if enrollments period is active --}}
-        @if ($settings->withinEnrollmentPeriod())
+    {{-- Show enrollment actions, if enrollments period is active --}}
+    @if ($settings->withinEnrollmentPeriod())
+        @if (! Auth::user()->student->isEnrolledInCourse($course))
+            {{-- Show button to enroll in course. --}}
+            <form action="{{ route('enrollments.create') }}" method="post">
+                {{ csrf_field() }}
+                <input type="hidden" name="course_id" value="{{ $course->id }}">
+                <button type="submit" class="btn btn-success btn-sm">Enroll in course</button>
+            </form>
+        @else
             <button type="button" class="btn btn-secondary btn-sm disabled">Enrolled</button>
             <button type="button" class="btn btn-secondary btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="caret"></span>
