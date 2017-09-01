29dd4682b26df2fa2811c14e0f0eaa59c2e51187
Francisco Neves <fntneves@gmail.com>
2017-09-01 23:08:28 +0100

    Leverage default parameter

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,8 +12,8 @@ class UsersTableSeeder extends Seeder
     {
         User::create([
             'name' => 'Administrator',
-            'email' => env('ADMIN_EMAIL') ?? 'contact@hackathonners.org',
-            'password' => bcrypt(env('ADMIN_PASSWORD') ?? '123456'),
+            'email' => env('ADMIN_EMAIL', 'contact@hackathonners.org'),
+            'password' => bcrypt(env('ADMIN_PASSWORD', '123456')),
             'is_admin' => true,
         ]);
     }
