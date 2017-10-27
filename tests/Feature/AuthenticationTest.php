f421c634d0dcc47426c2f9d2ae11fbca4fd196d3
Hugo Gonçalves <hmrvbg@gmail.com>
2017-10-28 00:19:37 +0100

    Override credentials method so that login email is converted to lower case

---
+++ tests/Feature/AuthenticationTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Feature;
+
+use Tests\TestCase;
+use App\Judite\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AuthenticationTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * @test
+     * @group bug7
+     **/
+    public function user_email_login_check_is_case_insensitve()
+    {
+        $user = factory(User::class)->create([
+                'email' => 'a7000@alunos.uminho.pt',
+                'password' => 'password',
+            ]);
+
+        $response = $this->post('/login', [
+                'email' => 'A7000@alunos.uminho.pt',
+                'password' => 'password',
+            ]);
+
+        $response->assertRedirect(route('dashboard'));
+    }
+}
