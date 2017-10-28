ae5b63be0b2ca06eb79e08c73040962bca16704a
Hugo Gonçalves <hmrvbg@gmail.com>
2017-10-28 17:15:42 +0100

    Fix indentation and route access in auth tests

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -14,14 +14,14 @@ class AuthenticationTest extends TestCase
     public function user_email_login_check_is_case_insensitve()
     {
         $user = factory(User::class)->create([
-                'email' => 'a7000@alunos.uminho.pt',
-                'password' => 'password',
-            ]);
+            'email' => 'a7000@alunos.uminho.pt',
+            'password' => 'password',
+        ]);
 
-        $response = $this->post('/login', [
-                'email' => 'A7000@alunos.uminho.pt',
-                'password' => 'password',
-            ]);
+        $response = $this->post(route('login'), [
+            'email' => 'A7000@alunos.uminho.pt',
+            'password' => 'password',
+        ]);
 
         $response->assertRedirect(route('dashboard'));
     }
