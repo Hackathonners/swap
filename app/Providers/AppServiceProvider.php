a1e20f77ebfff8fcec3ed1a64905993f625ba66c
Francisco Neves <fntneves@gmail.com>
2018-02-19 11:52:51 +0000

    Add missing facade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Judite\Models\Settings;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
