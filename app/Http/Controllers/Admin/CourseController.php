80565833cc26fee9517b2b4dd414127d552af0e8
João Vilaça <machadovilaca@gmail.com>
2018-07-22 22:08:09 +0100

    Update admin course controller

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -38,4 +38,29 @@ class CourseController extends Controller
 
         return view('students.index', $data);
     }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $course = Course::find($id);
+
+        $min = $request->input('min');
+        $max = $request->input('max');
+
+        if ($min > $max)
+            $max = $min;
+
+        $course->group_min = $min;
+        $course->group_max = $max;
+
+        $course->save();
+
+        return redirect()->route('admin.groups.index');
+    }
 }
