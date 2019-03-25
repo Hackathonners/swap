cc573f01a13445c8a7790dbf749f4b2b235e52e8
Hugo Matalonga <hmatalonga@gmail.com>
2019-03-25 22:32:01 +0000

    Fix element-ui default theme import

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -37,7 +37,7 @@ import ElementUI from 'element-ui';
 import CalendarEnrollments from './components/CalendarEnrollments.vue';
 import CalendarExchanges from './components/CalendarExchanges.vue';
 import FileInput from './components/FileInput.vue';
-import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-default/index.css';
 import locale from 'element-ui/lib/locale/lang/en'
 
 Vue.use(ElementUI, { locale });
